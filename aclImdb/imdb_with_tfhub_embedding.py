4cebb1a198f0814476413ccbb49ec148a20fce6d
Jonathan Hui <jonathan@jonathanhui.com>
2020-11-22 14:56:26 -0800

    Classifier with FC layers

---
+++ aclImdb/imdb_with_tfhub_embedding.py
@@ -0,0 +1,60 @@
+import os
+import numpy as np
+
+import tensorflow as tf
+import tensorflow_hub as hub
+import tensorflow_datasets as tfds
+
+# Load compressed models from tensorflow_hub
+os.environ["TFHUB_MODEL_LOAD_FORMAT"] = "COMPRESSED"
+
+print("Version: ", tf.__version__)
+print("Eager mode: ", tf.executing_eagerly())
+print("Hub version: ", hub.__version__)
+print("GPU is", "available" if tf.config.experimental.list_physical_devices("GPU") else "NOT AVAILABLE")
+
+# ds saved to ~/tensorflow_datasets/imdb_reviews
+# Split the training set into 60% and 40%, so we'll end up with 15,000 examples
+# for training, 10,000 examples for validation and 25,000 examples for testing.
+train_data, validation_data, test_data = tfds.load(
+    name="imdb_reviews",
+    split=('train[:60%]', 'train[60%:]', 'test'),
+    as_supervised=True)
+
+train_examples_batch, train_labels_batch = next(iter(train_data.batch(10)))
+print(train_examples_batch)
+print(train_labels_batch)
+
+# Load a pre-built embedding layer
+embedding = "https://tfhub.dev/google/nnlm-en-dim50/2"
+hub_layer = hub.KerasLayer(embedding, input_shape=[],
+                           dtype=tf.string, trainable=True)
+
+# print out the embeddings for 3 examples
+# (num_examples, embedding_dimension)
+result = hub_layer(train_examples_batch[:3])
+print(result)
+
+model = tf.keras.Sequential()
+model.add(hub_layer)
+model.add(tf.keras.layers.Dense(16, activation='relu'))
+model.add(tf.keras.layers.Dense(1))
+
+model.summary()
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+# Train with 10 epochs in mini-batches of shuffled 512 sample
+history = model.fit(train_data.shuffle(10000).batch(512),
+                    epochs=10,
+                    validation_data=validation_data.batch(512),
+                    verbose=1)
+
+results = model.evaluate(test_data.batch(512), verbose=2)
+
+# loss
+# accuracy
+for name, value in zip(model.metrics_names, results):
+    print("%s: %.3f" % (name, value))
